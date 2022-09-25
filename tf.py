6257c80fa7c1ed0ebc9a67f7b14667850c1dec34
ShiCheng Lu <shichenglu0708@gmail.com>
2022-09-25 00:09:36 -0400

    lint

--- tf.py
+++ tf.py
@@ -48,40 +48,51 @@ def load_data():
 
     return (x_train, y_train), (x_test, y_test)
 
+def create_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10)
+    ])
 
-(train_images, train_labels), (test_images, test_labels) = load_data()
+    model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(train_labels[i])
-plt.show()
+    return model
 
-train_images = train_images / 255.0
+def train_model(model, dataset, labels):
+    # train
+    model.fit(dataset, labels, epochs=10)
 
-test_images = test_images / 255.0
+def test_model(model, dataset, labels):
+    # test
+    test_loss, test_acc = model.evaluate(dataset,  labels, verbose=2)
 
+    print('\nTest accuracy:', test_acc)
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
-])
+def main():
+    (train_images, train_labels), (test_images, test_labels) = load_data()
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+    # display first 100 images
+    plt.figure(figsize=(10,10))
+    for i in range(25):
+        plt.subplot(5,5,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+        plt.xlabel(train_labels[i])
+    plt.show()
 
-model.fit(train_images, train_labels, epochs=10)
+    # normalize images colour to (0..1)
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+    model = create_model()
+    train_model(model, train_images, train_labels)
+    test_model(model, test_images, test_labels)
+    model.save("model")
 
-print('\nTest accuracy:', test_acc)
-
-model.save('model')
-
-tf.keras.Sequential()
+if __name__ == "__main__":
+    main()
